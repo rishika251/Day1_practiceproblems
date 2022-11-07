4180c05c070d19f5745ecd851a48035ff88fb6ab
rishika251 <rishikasadineni@gmail.com>
2022-11-07 08:14:23 +0530

    Added:ability to check given number is primitive or not

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.day1;
+
+public class PrimitiveDatatypes {
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static boolean bl;
+	static char c;
+
+	public static void main(String[] args) {
+		System.out.println("display the default value of all primitive data types of Java");
+		System.out.println("byte =" + b);
+		System.out.println("short =" + s);
+		System.out.println("int =" + i);
+		System.out.println("long =" + l);
+		System.out.println("float =" + f);
+		System.out.println("double =" + d);
+		System.out.println("boolean =" + bl);
+		System.out.println("char =" + c);
+
+	}
+
+}
