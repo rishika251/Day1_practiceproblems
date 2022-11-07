826ecbee8eab920ae9edead486efd2036207c417
rishika251 <rishikasadineni@gmail.com>
2022-11-07 08:12:07 +0530

    Added: ability to add Display a message

---
+++ DisplayAmessage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day1;
+
+public class DisplayAmessage {
+	public static void main(String[] args) {
+		// display any message
+		System.out.println("Welcome to Bridgelabz");
+
+	}
+
+}
